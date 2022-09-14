8e95d21a410af5e7e2a40d7c3519c62c3ad6725b
Hardi <98042516+MrHardix@users.noreply.github.com>
2022-09-14 04:08:05 -0700

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require("discord.js");
+const Discord = require("discord.js");
 const { Prefix } = require("../../config.js");
 
 module.exports = {
@@ -13,34 +13,46 @@ module.exports = {
     let helpEmbed = new MessageEmbed()
     .setFooter(message.author.username, message.author.displayAvatarURL)
     .setColor('F2254C')
-    .setTitle("Help Snow Music")
-    .setImage("")
-  .setDescription(`
-
-
-**User Commands**
-invite ,support ,about ,ping ,setprefix ,uptime.
-
-**Gif Commands**
-boy,girl,couple,sad,cartoon,anime
-
-**Music Commands**
-play,skip,skipto,stop,volume,nowplaying,Eshuffle,search ,resume.
-remove ,queue ,filter ,loop ,lyrics ,radio.
-
-
+    .setTitle("Help Command")
+    .setDescription(`
+ 
+︙**Info** 
+\`help\`, \`ping\`, \`Server\`,
+\`userinfo\`, \`invite\`, \`uptime\`
+\`say\`, \`uinvite\`, \`roles\`
+\`stats\`, \`listemoji\`, \`se <emoji>\`,
+︙**Moderation** 
+\`ban\`, \`unban\`, \`kick\`,
+\`mute\`, \`unmute\`, \`clear\`
+\`lock\`, \`unlock\`, \`slowmode\`
+\`nick\`, \`hide\`, \`unhide\` 
+︙**Funny**
+\`ascii\`, \`avatar\`, \`dicksize\`,
+\`howgay\`, \`rate\`, 
+︙**Gifs**
+\`boy\`, \`girl\`, \`anime\`,
+\`animal\`, \`baby\`, \`couple\`,
+\`emoji\`, 
+︙**Photos** 
+\`Pboy\`, \`Pgirl\`, \`Panime\`,
+\`Panimal\`, \`Pbaby\`, \`Pcouple\`,
+\`Pemoji\`
+︙**Texts**
+\`textkurd\` (tk) , \`textenglish\` (te)
+\`textarab\` (ta) , \`textturk\` (tt) 
+\`textpersian\` (tp)
 `)
-    
- const disbut = require("discord-buttons")
+
+const disbut = require("discord-buttons")
  let button = new disbut.MessageButton()
         .setStyle('url')
         .setLabel('Invite')
-        .setURL('https://discord.com/api/oauth2/authorize?client_id=779009713027022898&permissions=8&scope=bot');////سيرفرك
+        .setURL('https://discord.com/api/oauth2/authorize?client_id=776824647080738828&permissions=8&scope=bot');////سيرفرك
 
  let button1 = new disbut.MessageButton()
         .setStyle('url')
         .setLabel('Support')
-        .setURL(`https://discord.gg/PXWdGengNe`);
+        .setURL(`https://discord.gg/2YE6JQRDYc`);
 
     helpEmbed.setTimestamp();
 
