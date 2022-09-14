1a34558a3b39d33678b96cb1449705b7a9c6fcea
BlackMan✓ <78313191+BlackMan69x@users.noreply.github.com>
2022-09-14 13:30:32 +0300

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -1,20 +1,19 @@
 const Discord = require("discord.js");
-const { MessageEmbed } = require("discord.js");
 const { Prefix } = require("../../config.js");
 
 module.exports = {
   name: "help",
-  aliases: ["h", "Help"],
-  description: "Show Help Command",
-  usage: "Help",
-  run: async(client, message, args) => {
- 
-    
-    
-    let embed = new MessageEmbed()
-    .setColor("RANDOM")
-    .setTitle(`Help Renon!`)
-
+  aliases: ["h"],
+  description: "Display all commands and descriptions",
+  cooldown: 3,
+  edesc: "Type help to get a short preview of all Commands, Type help <COMMANDNAME> to get extended information about this one command!",
+  execute(message) {
+    let commands = message.client.commands.array();
+     
+    let helpEmbed = new MessageEmbed()
+    .setFooter(message.author.username, message.author.displayAvatarURL)
+    .setColor('F2254C')
+    .setTitle("Help Command")
     .setDescription(`
  
 ︙**Info** 
